909c1635c7042bdf06e4eac9b9f93ed7483fd0b5
jeejocj <jeejojoshy31@gmail.com>
2025-01-22 12:13:13 +0530

    small cahnge in user model

--- models/userModel.js
+++ models/userModel.js
@@ -15,7 +15,7 @@ const userSchema = new Schema({
     phone : {
         type : String,
         required: false,
-        unique: true,
+        // unique: true,
         sparse:true,
         default:null
     },
